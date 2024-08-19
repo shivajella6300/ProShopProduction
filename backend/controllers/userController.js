fab4f557a80883ec3c1f1fed4db527e8c52dcccf
shiva_jella <shivajella6300@gmail.com>
2024-08-19 09:16:26 +0530

    Changes in the env file and frontend and backend files here

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
   });
-  if (user) {
+  if (user) 
+    {
     generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
@@ -86,26 +87,23 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //@access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
-
   if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-
+    user.name =   req.body.name  || user.name;
+    user.email =  req.body.email || user.email;
     if (req.body.password) {
       user.password = req.body.password;
     }
 
     const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-    });
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+      });
   } else {
-    res.status(404);
-    throw new Error("User not found");
+      res.status(404);
+      throw new Error("User not found");
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-// import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 //Connected DataBase Runs Here;
@@ -20,12 +21,28 @@ app.use(cookieParser());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
 app.get("/api/config/paypal", (req, res) =>
   res.send({
     clinetId: process.env.PAYPAL_CLINET_ID,
   })
 );
+const __dirname = path.resolve(); //Set __dirname to current directory..
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
+  //any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
